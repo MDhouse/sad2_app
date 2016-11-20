791890d612e1e5e972730b66d0e1a921ac210636
Damian Junko <damian.junko@gmail.com>
2016-11-20 14:44:13 +0100

    Zbudowanie głównego layout'u

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,55 @@
 
 $(document).ready(function(){
-    $(".button-collapse").sideNav();
-    alert("Cześć");
+
+
+    $(".button-collapse").sideNav({
+        closeOnClick: true 
+    });
+
+     $('.dropdown-button').dropdown({      
+        belowOrigin: true, 
+        alignment: 'right' 
+    });
+
+////Strona głowna rózne warianty
+    $("#nav-mobile").on("click", "#linkIndex", function(e){
+        Index();
+    });
+    $("#navi").on("click", "#logo-container", function(e){
+        Index();
+    });
+     $("#naviLarge").on("click", "#linkIndexLarge", function(e){
+        Index();
+    });
+
+////// Odpalenie testu 13
+    $("#testBox").on("click", "#test13Box", function(e){
+        Test13();
+    });
+    $("#dropdownTest").on("click", "#dropTest13", function(e){
+        Test13();
+    });
+    $("#nav-mobile").on("click", "#linkTest13", function(e){
+        Test13();
+    });
+
+
+
 });
+
+ 
+
+function Index(){
+    $("main .section").each(function () {            
+            $(this).addClass("hiddendiv");
+            //$("#" + e).removeClass("hiddendiv");
+            $("#index").removeClass("hiddendiv");         
+        });
+};
+
+function Test13(){
+    $("main .section").each(function () {            
+            $(this).addClass("hiddendiv");
+            $("#test13").removeClass("hiddendiv");         
+        });
+}
